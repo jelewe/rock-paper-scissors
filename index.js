fcece8d0da5d95d986680bfc2a14f1274558d8cc
Jessie <hire.jesslw@gmail.com>
2022-08-25 10:11:51 -1000

    add scorekeeper function and define how many rounds to play

--- index.js
+++ index.js
@@ -8,38 +8,47 @@ function getComputerChoice() {
     return computerChoice
 }
 
+let computerScore = 0;
+let playerScore = 0;
+
 function playRound() {
     let playerSelection = prompt("What's your choice - rock, paper, or scissors?")
     playerSelection = playerSelection.toLowerCase().trim();
     let computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
         alert("It's a tie!");
-        return 0;
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         alert("You lose! Paper covers rock!")
-        return -1;
+        return computerScore++
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         alert("You win! Rock smashes scissors!")
-        return 1;
+        return playerScore++;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         alert("You win! Paper covers rock!")
-        return 1;
+        return playerScore++
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         alert("You lose! Scissors cut paper!")
-        return -1;
+        return computerScore++;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         alert("You lose! Rock smashes scissors!")
-        return -1;
+        return computerScore++;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         alert("You win! Scissors cut paper!")
-        return 1;
+        return playerScore++;
     }
 }
 
 function game() {
-    alert("Let's play rock, paper, scissors! Best score out of 5 rounds is the winner!")
+    alert("Let's play rock, paper, scissors! Best out of 5 wins!")
     for (let i = 1; i <= 5; i++) {
         playRound()
+    }
+    if (computerScore > playerScore) {
+        alert(`Computer: ${computerScore}, You: ${playerScore} --- You lose!`)
+    } else if (playerScore > computerScore) {
+        alert(`Computer: ${computerScore}, You: ${playerScore} --- You win!`)
+    } else if (computerScore === playerScore) {
+        alert(`Computer: ${computerScore}, You: ${playerScore} --- It's a tie!!!`)
     }
 }
 
