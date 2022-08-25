1d1872f2ed26479b89f2e4e1e8df217eec071473
Jessie <hire.jesslw@gmail.com>
2022-08-25 08:39:09 -1000

    create playRound function

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 function getComputerChoice() {
     let myArray = [
-        "Rock",
-        "Paper",
-        "Scissors"
+        "rock",
+        "paper",
+        "scissors"
     ];
     let computerChoice = myArray[Math.floor(Math.random()*myArray.length)];
     return computerChoice
 }
 
-console.log(getComputerChoice())
+function playRound() {
+    let playerSelection = prompt("What's your choice - rock, paper, or scissors?")
+    playerSelection = playerSelection.toLowerCase().trim();
+    let computerSelection = getComputerChoice();
+    if (playerSelection === computerSelection) {
+        alert("It's a tie!");
+    } else {
+        alert("You lose!")
+    }
+}
+
+playRound()
