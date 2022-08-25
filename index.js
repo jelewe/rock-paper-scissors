752f96476b79bf00824b43f517a84a5f760ee676
Jessie <hire.jesslw@gmail.com>
2022-08-25 09:16:54 -1000

    Create gamefunction to play 5 rounds

--- index.js
+++ index.js
@@ -14,19 +14,33 @@ function playRound() {
     let computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
         alert("It's a tie!");
+        return 0;
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         alert("You lose! Paper covers rock!")
+        return -1;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         alert("You win! Rock smashes scissors!")
+        return 1;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         alert("You win! Paper covers rock!")
+        return 1;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         alert("You lose! Scissors cut paper!")
+        return -1;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         alert("You lose! Rock smashes scissors!")
+        return -1;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         alert("You win! Scissors cut paper!")
+        return 1;
     }
 }
 
-playRound()
+function game() {
+    alert("Let's play rock, paper, scissors! Best score out of 5 rounds is the winner!")
+    for (let i = 1; i <= 5; i++) {
+        playRound()
+    }
+}
+
+game()
